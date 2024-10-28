2a0dca538830df6a739e108680cd55621c353c38
Muhammad Ghazy Humaidi <muhammad.ghazy.hum@gmail.com>
2024-10-28 23:13:03 +0700

    change file

--- jadwalKereta.java
+++ jadwalKereta.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class nyobaTobi {
+public class jadwalKereta {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[][] jadwal = new String[5][5];
