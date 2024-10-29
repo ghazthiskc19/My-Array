47eb144b2348c21e5d9220726da45e1725fd71af
Muhammad Ghazy Humaidi <muhammad.ghazy.hum@gmail.com>
2024-10-29 12:55:55 +0700

    create major change

--- MGhazyHumaidi2.java
+++ MGhazyHumaidi2.java
@@ -1,42 +1,24 @@
 import java.util.Scanner;
+
 public class MGhazyHumaidi2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] jadwal = new String[5][5];
-        jadwal[0][0] = "Jenis";
-        jadwal[0][1] = "     Harga";
-        jadwal[0][2] = "  Diskon";
-        jadwal[0][3] = "       AC";
-        jadwal[0][4] = "       Colokan";
-        jadwal[1][0] = "Ekonomi";
-        jadwal[1][1] = "   50000";
-        jadwal[1][2] = "   2% ";
-        jadwal[1][3] = "       Tidak ada";
-        jadwal[1][4] = "   Tidak ada";
-        jadwal[2][0] = "Bisnis";
-        jadwal[2][1] = "    100000";
-        jadwal[2][2] = "  5% ";
-        jadwal[2][3] = "         Ada";
-        jadwal[2][4] = "       Tidak ada";
-        jadwal[3][0] = "Eksekutif";
-        jadwal[3][1] = " 200000";
-        jadwal[3][2] = "  7% ";
-        jadwal[3][3] = "         Ada";
-        jadwal[3][4] = "       Tidak ada";
-        jadwal[4][0] = "Pariwisata";
-        jadwal[4][1] = "300000";
-        jadwal[4][2] = "  10% ";
-        jadwal[4][3] = "        Ada";
-        jadwal[4][4] = "       Ada";
-        int input = 0;
+        String[][] jadwal = {
+            {"Jenis", "     Harga", "  Diskon", "       AC", "       Colokan"},
+            {"Ekonomi", "   50000", "   2%", "       Tidak ada", "   Tidak ada"},
+            {"Bisnis", "    100000", "  5%", "         Ada", "       Tidak ada"},
+            {"Eksekutif", " 200000", "  7%", "         Ada", "       Tidak ada"},
+            {"Pariwisata", "300000", "  10%", "        Ada", "       Ada"}
+        };
+
+        int input;
         int std = 0;
-        String[] orderr = new String[100];
+        String[] pesanan = new String[100];
         int[] qty = new int[100];
-        
         int start = 0;
-        int end = 0;
+
         do {
-            System.out.println("1. Melihat Daftar Kereta Api");
+            System.out.println("\n1. Melihat Daftar Kereta Api");
             System.out.println("2. Melihat Kereta Api yang ada AC");
             System.out.println("3. Melihat Kereta Api yang ada Colokan");
             System.out.println("4. Memesan Tiket Kereta Api");
@@ -46,69 +28,74 @@ public class MGhazyHumaidi2 {
             input = sc.nextInt();
 
             switch (input) {
-                case 1:
-                    for (int i = 0; i < jadwal.length; i++) {
-                        for (int j = 0; j < jadwal.length; j++) {
-                            System.out.print(jadwal[i][j] + " ");
-                        }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case 2:
-                    for (int i = 0; i < jadwal.length; i++) {
-                        for (int j = 0; j < jadwal.length; j++) {
-                            if (i == 1) {
-                                continue;
-                            } else System.out.print(jadwal[i][j] + " ");
-                        }
-                        if (i == 1) {
-                            continue;
-                        } else System.out.println();
-
-                    }
-                    break;
-                case 3:
-                    for (int i = 0; i < jadwal.length; i++) {
-                        for (int j = 0; j < jadwal.length; j++) {
-                            if (i == 1 || i == 2 || i == 3) {
-                                continue;
-                            } else System.out.print(jadwal[i][j] + " ");
-                        }
-                        if (i == 1 || i == 2 || i == 3) {
-                            continue;
-                        } else System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case 4:
-                    for (int i = start; i < orderr.length; i++) {
-                        System.out.print("Jenis: ");
-                        String jenis = sc.next();
-                        
-                        System.out.print("Jumlah: ");
-                        int jumlah = sc.nextInt();
-                        std++;
-                        if (jenis.equals("-") || jumlah == 0) {
-                            std--;
-                            start = i;
-                            break;
-                        }
-                        orderr[i] = jenis;
-                        qty[i] = jumlah;
-                    }
-                    System.out.println(std);
-                    break;
-                case 5:
-                    System.out.println("List Pesanan: ");
-                    for (int i = 0; i < std; i++) {
-                        System.out.print(orderr[i] + "\t" + qty[i] + "\n");
-                    }
-                    break;
-                case 0:
+                case 1 -> {
+                    printJadwal(jadwal);
+                }
+                case 2 -> {
+                    printFilteredJadwal(jadwal, "Ada", 3); // Filter AC
+                }
+                case 3 -> {
+                    printFilteredJadwal(jadwal, "Ada", 4); // Filter Colokan
+                }
+                case 4 -> {
+                    std = buatPesanan(sc, pesanan, qty, start, std);
+                    start += std;
+                }
+                case 5 -> {
+                    lihatPesanan(pesanan, qty, std);
+                }
+                case 0 -> {
                     System.out.println("Anda memilih untuk keluar.\n");
-                    return;
+                }
+                default -> {
+                    System.out.println("Pilihan tidak valid.");
+                }
             }
         } while (input != 0);
+    }
+
+    private static void printJadwal(String[][] jadwal) {
+        for (String[] row : jadwal) {
+            for (String cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printFilteredJadwal(String[][] jadwal, String filter, int colIndex) {
+        for (int i = 0; i < jadwal.length; i++) {
+            if (i == 0 || jadwal[i][colIndex].contains(filter)) {
+                for (String cell : jadwal[i]) {
+                    System.out.print(cell + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    private static int buatPesanan(Scanner sc, String[] pesanan, int[] qty, int start, int std) {
+        System.out.println("Masukkan jenis dan jumlah tiket (ketik '-' atau '0' untuk keluar): ");
+        for (int i = start; i < pesanan.length; i++) {
+            System.out.print("Jenis: ");
+            String jenis = sc.next();
+            System.out.print("Jumlah: ");
+            int jumlah = sc.nextInt();
+
+            if ("-".equals(jenis) || jumlah == 0) {
+                break;
+            }
+            pesanan[i] = jenis;
+            qty[i] = jumlah;
+            std++;
+        }
+        return std;
+    }
+
+    private static void lihatPesanan(String[] pesanan, int[] qty, int std) {
+        System.out.println("List Pesanan: ");
+        for (int i = 0; i < std; i++) {
+            System.out.println(pesanan[i] + "\t" + qty[i]);
+        }
     }
 }
